c48ee0f3928332e6f0458f0635cfb3007d6a86a4
minanadyfawzy <98706088+minanadyfawzy@users.noreply.github.com>
2022-12-14 12:48:23 +0200

    Add files via upload

---
+++ multi.js
@@ -0,0 +1,46 @@
+function myFunction() {
+    var x = document.getElementById("menu1");
+    var m =document.getElementById("nav-3")
+    if (m.style.display === "none") {
+      m.style.display = "inline";
+      
+      } else {
+        m.style.display = "none";
+       
+      }
+    }
+
+    function show() {
+        var x = document.getElementById("nnnn");
+        var m =document.getElementById("show")
+        if (m.style.display === "none") {
+          m.style.display = "flex";
+          
+          } else {
+            m.style.display = "none";
+           
+          }
+        }
+        function show1() {
+            var x = document.getElementById("nnnn");
+            var m =document.getElementById("show1")
+            if (m.style.display === "none") {
+              m.style.display = "flex";
+              
+              } else {
+                m.style.display = "none";
+               
+              }
+            }
+            function show2() {
+                var x = document.getElementById("nnnn");
+                var m =document.getElementById("show2")
+                if (m.style.display === "none") {
+                  m.style.display = "flex";
+                  
+                  } else {
+                    m.style.display = "none";
+                   
+                  }
+                }
+      
